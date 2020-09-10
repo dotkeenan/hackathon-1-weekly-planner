8fab28fdb0e16af181080be78e5927c67371c491
Keenan Ng <keenandng@gmail.com>
2020-09-10 12:51:00 -0700

    created function to dynamic create tr and td

--- main.js
+++ main.js
@@ -191,7 +191,10 @@ weekDaysContainer.addEventListener("click", getDay);
 // define function that stores the event.target (day clicked)
 function getDay(event) {
   var targetDay = event.target;
+  var targetDayId = targetDay.getAttribute('id');
   var targetDayText = targetDay.textContent;
+  dayName = targetDayId;
+  addTableEntry();
   // console.log('event.target:', targetDay)
   // Exclude clicks that aren't on the actual .dayItem's
   if (event.target.className.indexOf("dayItem") === -1) {
@@ -213,8 +216,23 @@ var submitButton = document.getElementById("submitButton");
 submitButton.addEventListener('click', getEntryData);
 function getEntryData(event)  {
   // Storing the text values of modal input elements
-  var daySelectValue = daySelect.options[daySelect.selectedIndex].textContent;
+  var daySelectValue = daySelect.options[daySelect.selectedIndex].value;
   var timeSelectValue = timeSelect.options[timeSelect.selectedIndex].textContent;
   var inputBoxValue = inputBox.value;
   // ↓↓ Code for pushing to weekDaysArray and appending to table goes below? ↓↓
+  // not sure if this is right
+  getDay(event);
+}
+
+//Function to dynamically create tr and td
+function addTableEntry() {
+  var tableBody = document.getElementById('tableBody');
+  var tableRow = document.createElement('tr');
+  var tableDataTime = document.createElement('td');
+  var tableDataTask = document.createElement('td');
+
+  tableDataTime.textContent = dayScheduleArr[dayName].time;
+  tableDataTask.textContent = dayScheduleArr[dayName].description;
+  tableRow.append(tableDataTime, tableDataTask);
+  tableBody.append(tableRow);
 }
