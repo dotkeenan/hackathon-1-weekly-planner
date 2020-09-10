b5a49c8ad9f577a02970be86fbd304e847165b52
Keenan Ng <keenandng@gmail.com>
2020-09-09 18:43:42 -0700

    feature2 added event listener to the #weekDayContainer, created getDay function to store event.target.textContent and the event.target

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+// dom query for #weekDaysContainer & add event listener
+var weekDaysContainer = document.getElementById('weekDaysContainer');
+weekDaysContainer.addEventListener('click', getDay);
+// define function that stores the event.target (day clicked)
+function getDay(event) {
+  var targetDay = event.target;
+  var targetDayText = targetDay.textContent;
+  // console.log('event.target:', targetDay)
+  // Exclude clicks that aren't on the actual .dayItem's
+  if (event.target.className.indexOf('dayItem') === -1) {
+    return
+  } else {
+    // console.log('targetDay textContent:', targetDayText);
+    return targetDayText;
+  }
+}
